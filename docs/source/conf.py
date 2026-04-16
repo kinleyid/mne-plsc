25f3f491dd1bf2cd698adfb9af9cb7e90980d0ce
Isaac Kinley <isaac.kinley@gmail.com>
2026-04-15 20:07:20 -0400

    set up for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,22 @@ author = 'Isaac Kinley'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+	'sphinx.ext.autodoc',
+	'sphinx.ext.napoleon',
+	'nbsphinx'
+]
+
+napoleon_numpy_docstring = True
+napoleon_use_ivar = False
+
+# nbsphinx_execute = "never"
+nbsphinx_execute = "always"
+exclude_patterns = ["build", "**.ipynb_checkpoints"]
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
 
 templates_path = ['_templates']
 exclude_patterns = []
