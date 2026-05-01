5a7946b4ce9abc058c4f6e2da4a4e60be1bc2f27
Isaac Kinley <isaac.kinley@gmail.com>
2026-05-01 12:03:06 -0400

    Stop trying to render 3d surface

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,13 +48,3 @@ html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
 
 nbsphinx_allow_errors = True
-
-# Set up for static rendering of surface images
-import pyvista as pv
-from mne.viz._brain import _BrainScraper as BrainScraper
-pv.set_plot_theme("document")
-pv.OFF_SCREEN = True
-os.environ["_MNE_BUILDING_DOC"] = "true"
-sphinx_gallery_conf = {
-    "image_scrapers": ("matplotlib", BrainScraper, "pyvista")
-}
